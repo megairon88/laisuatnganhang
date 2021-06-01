b3787950c6c2f7bbcbbcab5c4517a7975ec31726
megairon88 <megairon88@gmail.com>
2021-06-02 00:31:15 +0900

    all all file

---
+++ my.js
@@ -0,0 +1,5 @@
+let money= +prompt("so tien vay:");
+let interest_rate= +prompt("lai suat nam:");
+let time= +prompt("so nam");
+sum= money+money*(interest_rate/100)*time;
+document.write("so tien phai tra" +sum+ "VND")
